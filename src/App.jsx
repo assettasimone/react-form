7ade9f36f2fb48293bab3d538a74946a5221fee5
assettasimone <assettasimone@gmail.com>
2025-10-06 19:25:53 +0200

    Refactor post submission logic and update form to handle new post titles

--- src/App.jsx
+++ src/App.jsx
@@ -3,11 +3,32 @@ import blogPost from './data/blogPost'
 
 
 function App() {
-  const [name, setName] = useState('')
+  const [newPostTitle, setNewPostTitle] = useState('')
+  const [posts, setPosts] = useState(blogPost)
+
+
 
   const handleSubmit = e => {
     e.preventDefault()
-    alert('form submitted ' + name)
+
+    const newPost = {
+      id: 1,
+      title: newPostTitle,
+      metaDescription: '',
+      author: '',
+      Tag: []
+    }
+
+    if (newPostTitle.length >= 5) {
+
+
+      setPosts([...posts, newPost])
+      setNewPostTitle('')
+    }
+    else {
+      alert('Il titolo deve essere almeno 5 caratteri')
+    }
+
   }
 
 
@@ -16,8 +37,8 @@ function App() {
       <div className='container mt-4'>
         <form action="" onSubmit={handleSubmit}>
           <div className="input-group mb-3">
-            <span className="input-group-text" id="basic-addon1">Name</span>
-            <input type="text" className="form-control" placeholder="Username" aria-label="Username" aria-describedby="basic-addon1" value={name} onChange={e => { setName(e.target.value) }} />
+            <span className="input-group-text" id="basic-addon1">New Post Title</span>
+            <input type="text" className="form-control" placeholder="Insert a new post title here" aria-label="Username" aria-describedby="basic-addon1" value={newPostTitle} onChange={e => { setNewPostTitle(e.target.value) }} />
           </div>
           <button className='btn btn-primary'>Invia</button>
         </form>
@@ -25,7 +46,7 @@ function App() {
       <div className="container">
         <h2 className='my-4'>Lista Articoli</h2>
         <ul className="list-group">
-          {blogPost.map(post => (
+          {posts.map(post => (
             <li className="list-group-item">
               {post.title}
             </li>
